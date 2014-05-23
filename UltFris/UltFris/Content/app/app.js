a3f53cb0a04eed2ef128bc6dadefed93a67c5e10
Arnold <sh8erby@knights.ucf.edu>
2014-05-22 22:48:30 -0400

    worked on create page

---
+++ UltFris/UltFris/Content/app/app.js
@@ -0,0 +1,23 @@
+﻿angular.module("myapp", ["firebase"])
+        .value('fbAuthToken', 'KVHxq4JCUBdBAfy7Aa3M6ca0mEq9ZE2zqltgI83e')
+        .value('fbURL', 'https://ultfris.firebaseio.com');
+
+function MyController($scope, $firebase) {
+
+    var ref = new Firebase("https://ultfris.firebaseio.com");
+    $scope.messages = $firebase(ref);
+
+    //keycode 13 is enter
+    $scope.addMessage = function (e) {
+        if (e.keyCode != 13) return;
+        $scope.messages.$add({ from: $scope.name, body: $scope.msg });
+        $scope.msg = "";
+    }
+
+    $scope.addMess = function (e) {
+        $scope.messages.$add({ from: $scope.name, body: $scope.msg });
+        $scope.msg = "";
+    }
+}
+       //.value('fbAuthToken', 'KVHxq4JCUBdBAfy7Aa3M6ca0mEq9ZE2zqltgI83e')
+       //.value('fbURL', 'https://ultfris.firebaseio.com/');
--- UltFris/UltFris/Content/app/index.js
+++ UltFris/UltFris/Content/app/index.js
@@ -1,1 +1,6 @@
-﻿
+﻿var project = angular.module('project')
+
+project.controller('MyCtrl', function($scope, test)
+{
+     $scope.messages = test;
+});
