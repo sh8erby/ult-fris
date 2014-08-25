bbf98e49e69a693049379dc5e2b4861f24439af4
Arnold <sh8erby@knights.ucf.edu>
2014-08-25 15:20:12 -0400

    sigh

--- UltFris/UltFris/js/js_routes.js
+++ UltFris/UltFris/js/js_routes.js
@@ -2,9 +2,9 @@ angular.module ('project')
 	   .config(function($routeProvider)  
 			{
 	       $routeProvider
-              .when('/messages', {templateUrl: 'views/messages.html', controller: 'MessagesCtrl'})             
-              .when('/create', { templateUrl: 'views/create.html', controller: 'CreateCtrl' })
+              .when('/messages', {templateUrl: 'views/messages.html', controller: 'MessagesCtrl'})
               .when('/roster', { templateUrl: 'views/roster.html', controller: 'RosterCtrl' })
+              .when('/create', { templateUrl: 'views/create.html', controller: 'CreateCtrl' })
               .when('/queue', {templateUrl: 'views/queue.html', controller: 'QueueCtrl'})
               .when('/edit', { templateUrl: 'views/edit.html' })
     	      .otherwise({ redirectTo  :'/messages'});
